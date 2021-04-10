8f279dc3b67bcee5d22d698eb0b49685e3d72fe5
Niko <bluepspower@163.com>
2021-04-11 01:40:16 +0800

    chore: update copyright

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
     return (
         <section className="col-md-12 card-wrapper">
-            <div className="footer">Copyright &copy; 2021-present by [ <code>vite-react-resume-poc</code> ]. All rights reserved.</div>
+            <div className="footer">Copyright &copy; 2021-present by [ <code>v-poc</code> ]. All rights reserved.</div>
         </section>
     )
 }
